5a3a94ebf6721c572333e36f6130474239e84338
evolentDelight <evolentDelight@outlook.com>
2022-10-23 20:42:22 -0700

    completed code

--- Back End Development/2.7 Resolving Promises Starter Code/src/main.js
+++ Back End Development/2.7 Resolving Promises Starter Code/src/main.js
@@ -2,14 +2,29 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
 const promise = welcome();
 
-promise.then((response) => console.log(response + "\n\n"));
-
 const question = "Will the weather be nice today?";
 
-const tellPromise = tell(question);
+promise
+  .then((response) => console.log(response + "\n\n" + question)) // Logs the result of welcome()
 
-tellPromise.then((fortune) => {
-  console.log(question);
+  .then(tell(question).then(console.log).catch(console.error))
 
-  console.log(fortune);
-});
+  .catch(console.error)
+
+  .then(goodbye) // Returns promise from goodbye()
+
+  .then(console.log) // Logs the result from goodbye()
+
+  .catch(console.error); // Logs error only from goodbye()
+
+// promise.then((response) => console.log(response + "\n\n"));
+
+// const tellPromise = tell(question);
+
+// tellPromise
+//   .then((fortune) => {
+//     console.log(question);
+
+//     console.log(fortune);
+//   })
+//   .catch(console.error);
