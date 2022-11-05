8d47f9d977d0a5cd824ed8ae2306c520e9b7ed6a
evolentDelight <evolentDelight@outlook.com>
2022-11-05 14:10:27 -0700

    Routes&Parameters with Order of Code

--- Back End Development/2.2.2 node-express-getting-started/src/app.js
+++ Back End Development/2.2.2 node-express-getting-started/src/app.js
@@ -4,10 +4,28 @@ const morgan = require("morgan");
 const app = express();
 
 const sayHello = (req, res, next) => {
-  res.send("Hello!");
+  console.log(req.query);
+
+  const name = req.query.name;
+
+  const content = name ? `Hello, ${name}!` : "Hello!";
+  res.send(content);
+};
+
+const saySomething = (req, res) => {
+  const greeting = req.params.greeting;
+  const name = req.query.name;
+  const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}`;
+  res.send(content);
 };
 
 app.use(morgan("dev"));
 app.get("/hello", sayHello);
+app.get("/say/:greeting", saySomething);
+
+app.get("/songs", (req, res) => {
+  const title = req.query.title;
+  res.send(title);
+});
 
 module.exports = app;
