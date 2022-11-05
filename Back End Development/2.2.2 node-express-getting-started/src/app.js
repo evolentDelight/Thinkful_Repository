56b2cbc04642c108c83a266dc941f362e0f01514
evolentDelight <evolentDelight@outlook.com>
2022-11-05 14:27:56 -0700

    Error handler: Catch all & Express's error handling middleware

--- Back End Development/2.2.2 node-express-getting-started/src/app.js
+++ Back End Development/2.2.2 node-express-getting-started/src/app.js
@@ -3,6 +3,18 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.use(morgan("dev"));
+
+app.get("/states/:abbreviation", (req, res, next) => {
+  const abbreviation = req.params.abbreviation;
+
+  if (abbreviation.length != 2) {
+    next("State abbreviation is invalid");
+  } else {
+    res.send(`${abbreviation} is a nice state.`);
+  }
+});
+
 const sayHello = (req, res, next) => {
   console.log(req.query);
 
@@ -19,13 +31,24 @@ const saySomething = (req, res) => {
   res.send(content);
 };
 
-app.use(morgan("dev"));
 app.get("/hello", sayHello);
 app.get("/say/:greeting", saySomething);
 
 app.get("/songs", (req, res) => {
   const title = req.query.title;
   res.send(title);
+});
+
+app.use((req, res, next) => {
+  //Error handling by placing this function last
+  res.send(`The route ${req.path} does not exist!`);
+});
+
+app.use((err, req, res, next) => {
+  //Express middleware error handling by having FOUR parameters. must be four
+  //Other than triggered by order, it can also be called using next() but there must be parameters in next().
+  console.error(err);
+  res.send(err);
 });
 
 module.exports = app;
