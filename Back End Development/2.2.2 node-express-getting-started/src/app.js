df79a992e37d7081e0f46154c219b28e702a6536
evolentDelight <evolentDelight@outlook.com>
2022-11-05 13:30:44 -0700

    middleware+morganLogging

--- Back End Development/2.2.2 node-express-getting-started/src/app.js
+++ Back End Development/2.2.2 node-express-getting-started/src/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+const sayHello = (req, res, next) => {
+  res.send("Hello!");
+};
+
+app.use(morgan("dev"));
+app.use(sayHello);
 
 module.exports = app;
