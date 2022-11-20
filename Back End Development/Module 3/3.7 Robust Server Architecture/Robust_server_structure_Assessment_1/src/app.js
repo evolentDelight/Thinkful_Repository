63cab89202bf6f0b772aaab9b5b3c8e1d2e41bcc
evolentDelight <evolentDelight@outlook.com>
2022-11-20 09:18:04 -0800

    Error Handler in app

--- Back End Development/Module 3/3.7 Robust Server Architecture/Robust_server_structure_Assessment_1/src/app.js
+++ Back End Development/Module 3/3.7 Robust Server Architecture/Robust_server_structure_Assessment_1/src/app.js
@@ -6,4 +6,15 @@ app.use(express.json());
 
 // TODO: Add code to meet the requirements and make the tests pass.
 
+//Error:Catch-all
+app.use((req, res, next) => {
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong!" } = err;
+
+  res.status(status).json({ error: message });
+});
+
 module.exports = app;
