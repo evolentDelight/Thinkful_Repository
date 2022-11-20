baed14d80037b93e247bb6ff31bb60c17419310f
evolentDelight <evolentDelight@outlook.com>
2022-11-19 17:00:17 -0800

    Reorganize and achieve simplicity

--- Back End Development/Module 3/0 starter-robust-server-structure-paste-main/src/pastes/pastes.controller.js
+++ Back End Development/Module 3/0 starter-robust-server-structure-paste-main/src/pastes/pastes.controller.js
@@ -4,6 +4,42 @@ function list(req, res) {
   res.json({ data: pastes });
 }
 
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+
+  if (text) {
+    return next();
+  }
+
+  next({
+    status: 400,
+    message: "A 'text' property is required.",
+  });
+}
+
+function create(req, res) {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+    req.body;
+
+  const newPaste = {
+    id: ++lastPasteId, // Increment last id then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+
+  pastes.push(newPaste);
+
+  res.status(201).json({ data: newPaste });
+}
+
 module.exports = {
+  create: [bodyHasTextProperty, create],
+
   list,
 };
