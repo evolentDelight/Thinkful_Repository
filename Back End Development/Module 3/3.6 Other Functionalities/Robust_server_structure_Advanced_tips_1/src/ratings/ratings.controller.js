36372c3604295b1e62049a899913d3ea3e83a925
evolentDelight <evolentDelight@outlook.com>
2022-11-19 20:44:05 -0800

    Implementation of finding ONE rating

--- Back End Development/Module 3/3.6 Other Functionalities/Robust_server_structure_Advanced_tips_1/src/ratings/ratings.controller.js
+++ Back End Development/Module 3/3.6 Other Functionalities/Robust_server_structure_Advanced_tips_1/src/ratings/ratings.controller.js
@@ -4,6 +4,25 @@ function list(req, res, next) {
   res.json({ data: ratings });
 }
 
+function ratingExists(req, res, next) {
+  const { ratingId } = req.params;
+  const foundRating = ratings.find((rating) => rating.id === Number(ratingId));
+  if (foundRating) {
+    res.locals.rating = foundRating;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Rating not found: ${ratingId}`,
+  });
+}
+
+function read(req, res, next) {
+  res.json({ data: res.locals.rating });
+}
+
 module.exports = {
   list,
+  read: [ratingExists, read],
+  ratingExists,
 };
--- Back End Development/Module 3/3.6 Other Functionalities/Robust_server_structure_Advanced_tips_1/src/ratings/ratings.router.js
+++ Back End Development/Module 3/3.6 Other Functionalities/Robust_server_structure_Advanced_tips_1/src/ratings/ratings.router.js
@@ -3,7 +3,7 @@ const controller = require("./ratings.controller");
 
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-//router.route("/:ratingId").get().all(methodNotAllowed);
+router.route("/:ratingId").get(controller.read).all(methodNotAllowed);
 
 router.route("/").get(controller.list).all(methodNotAllowed);
 
