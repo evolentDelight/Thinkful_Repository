5116f8688b5c95626df84c70ecb57139bdb7629a
evolentDelight <evolentDelight@outlook.com>
2022-11-19 20:04:28 -0800

    Error via All

--- Back End Development/Module 3/0 starter-robust-server-structure-paste-main/src/users/users.router.js
+++ Back End Development/Module 3/0 starter-robust-server-structure-paste-main/src/users/users.router.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-
 const controller = require("./users.controller");
+const methodNotAllowed = requre("../errors/methodNotAllowed.js");
 
-router.route("/:userId").get(controller.read);
+router.route("/:userId").get(controller.read).all(methodNotAllowed);
 
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
