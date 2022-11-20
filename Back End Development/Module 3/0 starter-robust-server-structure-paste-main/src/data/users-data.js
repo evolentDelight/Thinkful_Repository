33f7e7b43c5c0b19c24a73a4b416bca7e6a5b237
evolentDelight <evolentDelight@outlook.com>
2022-11-19 19:22:11 -0800

    Users Data

---
+++ Back End Development/Module 3/0 starter-robust-server-structure-paste-main/src/data/users-data.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    id: 1,
+    username: "shakira",
+  },
+
+  {
+    id: 2,
+    username: "coltrane",
+  },
+
+  {
+    id: 3,
+    username: "beethoven",
+  },
+
+  {
+    id: 4,
+    username: "baesuzy",
+  },
+];
