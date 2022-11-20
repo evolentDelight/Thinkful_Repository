d049edeedf7850453cd4ee20a09a52486e8cea4c
evolentDelight <evolentDelight@outlook.com>
2022-11-19 18:40:52 -0800

    CRUD

--- Back End Development/Module 3/3.5 Organizing Express code/Robust_server_structure_Organizing_Express_code_1/src/app.js
+++ Back End Development/Module 3/3.5 Organizing Express code/Robust_server_structure_Organizing_Express_code_1/src/app.js
@@ -10,25 +10,6 @@ app.use("/notes", notesRouter);
 
 app.use(express.json());
 
-const hasText = (req, res, next) => {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next();
-  }
-  return next({ status: 400, message: "A 'text' property is required." });
-};
-
-app.post("/notes", hasText, (req, res, next) => {
-  const { data: { text } = {} } = req.body;
-
-  const newNote = {
-    id: notes.length + 1, // Assign the next ID
-    text,
-  };
-  notes.push(newNote);
-  res.status(201).json({ data: newNote });
-});
-
 // Not-found handler
 app.use((req, res, next) => {
   return next({ status: 404, message: `Not found: ${req.originalUrl}` });
--- Back End Development/Module 3/3.5 Organizing Express code/Robust_server_structure_Organizing_Express_code_1/src/notes/notes.controller.js
+++ Back End Development/Module 3/3.5 Organizing Express code/Robust_server_structure_Organizing_Express_code_1/src/notes/notes.controller.js
@@ -26,9 +26,35 @@ function read(req, res, next) {
 }
 
 //Create Handler
+function hasText(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next();
+  }
+  return next({ status: 400, message: "A 'text' property is required." });
+}
+
+function create(req, res, next) {
+  const { data: { text } = {} } = req.body;
+
+  const newNote = {
+    id: notes.length + 1, // Assign the next ID
+    text,
+  };
+  notes.push(newNote);
+  res.status(201).json({ data: newNote });
+}
 
 //Update Handler
+function update(req, res, next) {}
 
 //Delete Handler
+function destroy(req, res, next) {}
 
-module.exports = { list, read: [noteExists, read] };
+module.exports = {
+  list,
+  read: [noteExists, read],
+  create: [hasText, create],
+  update: [noteExists, update],
+  delete: [noteExists, destroy],
+};
