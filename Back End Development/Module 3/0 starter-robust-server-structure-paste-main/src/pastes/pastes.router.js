034a7009295b56fe996a68edc551b584be40c601
evolentDelight <evolentDelight@outlook.com>
2022-11-19 17:16:24 -0800

    Commenting for better understanding

--- Back End Development/Module 3/0 starter-robust-server-structure-paste-main/src/pastes/pastes.router.js
+++ Back End Development/Module 3/0 starter-robust-server-structure-paste-main/src/pastes/pastes.router.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const controller = require("./pastes.controller");
 
 router.route("/").get(controller.list).post(controller.create);
+// ==
+// router.route("/").get(controller.list);
+// router.route("/").post(controller.create);
 
 module.exports = router;
