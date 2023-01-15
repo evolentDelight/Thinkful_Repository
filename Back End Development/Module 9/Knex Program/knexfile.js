461620d9ee214229021b704f7b914c236ba1e270
evolentDelight <evolentDelight@outlook.com>
2023-01-15 10:14:52 -0800

    Knex Migrations

--- Back End Development/Module 9/Knex Program/knexfile.js
+++ Back End Development/Module 9/Knex Program/knexfile.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
+const path = require("path");
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -8,5 +10,8 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
   },
 };
