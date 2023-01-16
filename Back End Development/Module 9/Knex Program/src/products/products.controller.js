480050e3e840276f06d23f8333662e54071f0eb8
evolentDelight <evolentDelight@outlook.com>
2023-01-16 14:59:12 -0800

    Product's total weight in lbs query

--- Back End Development/Module 9/Knex Program/src/products/products.controller.js
+++ Back End Development/Module 9/Knex Program/src/products/products.controller.js
@@ -32,9 +32,14 @@ async function listPriceSummary(req, res, next) {
   res.json({ data: await productsService.listPriceSummary() });
 }
 
+async function listTotalWeightByProduct(req, res, next) {
+  res.json({ data: await productsService.listTotalWeightByProduct() });
+}
+
 module.exports = {
   read: [asyncErrorBoundary(productExists), read],
   list: [asyncErrorBoundary(list)],
   listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
   listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
 };
